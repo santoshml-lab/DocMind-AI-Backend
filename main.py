864bb9a51a0563b535a253efab20790c2c1da1e6
santoshml-lab <santoshkrsbg36@gmail.com>
2026-08-16 23:28:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+import pdfplumber
+import io
 
 app = FastAPI(
     title="DocMind AI",
@@ -11,4 +13,42 @@ app = FastAPI(
 def home():
     return {
         "message": "DocMind AI Backend is running 🚀"
+    }
+
+
+@app.post("/upload")
+async def upload_pdf(file: UploadFile = File(...)):
+
+    if file.content_type != "application/pdf":
+        raise HTTPException(
+            status_code=400,
+            detail="Only PDF files are allowed."
+        )
+
+    file_bytes = await file.read()
+
+    extracted_text = ""
+    page_count = 0
+
+    try:
+        with pdfplumber.open(io.BytesIO(file_bytes)) as pdf:
+
+            page_count = len(pdf.pages)
+
+            for page in pdf.pages:
+                text = page.extract_text()
+
+                if text:
+                    extracted_text += text + "\n"
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"PDF processing failed: {str(e)}"
+        )
+
+    return {
+        "filename": file.filename,
+        "pages": page_count,
+        "text": extracted_text
     }
